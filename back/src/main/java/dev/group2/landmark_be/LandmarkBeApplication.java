65f3904675899c6c975f6a259abd9979baff8bd4
doopang24 <wodus4145@naver.com>
2025-10-03 00:37:36 +0900

    build(back): eGov 에서 Spring Boot 로 전환

---
+++ back/src/main/java/dev/group2/landmark_be/LandmarkBeApplication.java
@@ -0,0 +1,13 @@
+package dev.group2.landmark_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandmarkBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LandmarkBeApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/dev/group2/landmark_be/LandmarkBeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.group2.landmark_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandmarkBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

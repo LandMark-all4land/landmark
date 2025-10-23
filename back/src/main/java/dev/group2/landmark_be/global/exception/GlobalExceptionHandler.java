b2cf577d756d75bec9083d77e4b75b48efd36f95
doopang24 <wodus4145@naver.com>
2025-10-23 21:25:22 +0900

    feat: 전역 예외 처리 객체 생성

---
+++ back/src/main/java/dev/group2/landmark_be/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package dev.group2.landmark_be.global.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+}

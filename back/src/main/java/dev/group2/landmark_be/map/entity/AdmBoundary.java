cdebe5f539f9ce83cb2225c72380788496a8b7cd
doopang24 <wodus4145@naver.com>
2025-10-24 00:22:00 +0900

    feat: 엔티티 생성
    Landmark, LandmarkRaster, AdmBoundary

--- back/src/main/java/dev/group2/landmark_be/map/entity/AdmBoundary.java
+++ back/src/main/java/dev/group2/landmark_be/map/entity/AdmBoundary.java
@@ -1,4 +1,34 @@
 package dev.group2.landmark_be.map.entity;
 
+import org.geolatte.geom.MultiPolygon;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "adm_boundary", schema = "app")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class AdmBoundary {
+	@Id
+	@Column(name = "adm_code", length = 12)
+	private String admCode;
+
+	@Column(name = "adm_name", nullable = false, columnDefinition = "TEXT")
+	private String admName;
+
+	@Column(name = "level", nullable = false)
+	private Short level;
+
+	@Column(name = "geom", nullable = false)
+	private MultiPolygon geom;
 }

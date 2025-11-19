1b55c3a22bc7805c94d3e40f8c394976e8fb0c1c
doopang24 <wodus4145@naver.com>
2025-11-19 23:54:16 +0900

    feat: 사용자, 메모 관련 엔티티 구현

---
+++ back/src/main/java/dev/group2/landmark_be/auth/entity/User.java
@@ -0,0 +1,119 @@
+package dev.group2.landmark_be.auth.entity;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+enum AuthProvider {
+	GITHUB, GOOGLE, NONE
+}
+
+@Entity
+@Table(name = "users", schema = "app")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class User implements UserDetails, OAuth2User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	// oauth 제공자 - 깃헙, 구글 등
+	@Enumerated(EnumType.STRING)
+	@Column(name = "oauth_provider", nullable = false)
+	private AuthProvider oauthProvider;
+
+	// oauth provider 에서 받은 고유 id
+	@Column(name = "oauth_id", unique = true, nullable = false)
+	private String oauthId;
+
+	// 깃헙 닉네임
+	@Column(name = "username", nullable = false)
+	private String username;
+
+	@Column(name = "email", unique = true)
+	private String email;
+
+	// jwt에 저장할 수 있는 역할 정의
+	@Column(name = "role")
+	private String role;
+
+	// oauthAttributes 를 임시 저장하는 필드 (db에 저장되지 않음)
+	@Transient
+	private Map<String, Object> attributes;
+
+	@Override
+	public Map<String, Object> getAttributes() {
+		return this.attributes;
+	}
+
+	@Override
+	public String getName() {
+		return this.username;
+	}
+
+	// UserDetails 인터페이스 구현 (spring security 필수)
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Collections.singletonList(new SimpleGrantedAuthority(this.role));
+	}
+
+	@Override
+	public String getPassword() {
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.username;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	public User updateOAuthInfo(String username, String email, Map<String, Object> attributes) {
+		this.username = username;
+		this.email = email;
+		this.attributes = attributes;
+		return this;
+	}
+}
---
+++ back/src/main/java/dev/group2/landmark_be/note/entity/Note.java
@@ -0,0 +1,48 @@
+package dev.group2.landmark_be.note.entity;
+
+import java.time.LocalDateTime;
+
+import dev.group2.landmark_be.auth.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "notes", schema = "app")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Note {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+
+	@Column(name = "landmark_id", nullable = false)
+	private Long landmarkId;
+
+	@Column(name = "content", columnDefinition = "TEXT", nullable = false)
+	private String content;
+
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+}
